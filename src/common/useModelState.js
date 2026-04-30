2e2503d5b17ce2084e53f7a0f22ca8f9e366d5c2
Botzy <botzy@goodmorning.dev>
2026-04-30 19:04:23 +0300

    revert: useInsertionEffect instead useLayoutEffect

--- src/common/useModelState.js
+++ src/common/useModelState.js
@@ -32,17 +32,17 @@ const useModelState = ({ action, ...args }) => {
             }
         }
     );
-    React.useLayoutEffect(() => {
+    React.useInsertionEffect(() => {
         if (action) {
             core.transport.dispatch(action, model);
         }
     }, [action]);
-    React.useLayoutEffect(() => {
+    React.useInsertionEffect(() => {
         return () => {
             core.transport.dispatch({ action: 'Unload' }, model);
         };
     }, []);
-    React.useLayoutEffect(() => {
+    React.useInsertionEffect(() => {
         const onNewState = async (models) => {
             if (models.indexOf(model) === -1 && (!Array.isArray(deps) || intersection(deps, models).length === 0)) {
                 return;
@@ -67,7 +67,7 @@ const useModelState = ({ action, ...args }) => {
             core.transport.off('NewState', onNewStateThrottled);
         };
     }, [routeFocused]);
-    React.useLayoutEffect(() => {
+    React.useInsertionEffect(() => {
         mountedRef.current = true;
     }, []);
     return state;
